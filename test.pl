d81f494bf3c2b2aa3833401f1ea5bd49bac94038
Ben Tyler <benjamin.tyler@gmail.com>
2020-10-05 01:29:26 +0200

    Update test file.
    This is pretty great!
    
    Parsing:
    * use / no / require statements, including version numbers and qw() expressions.
    * string literals (as long as they only have subname-compatible characters inside them...)
    * sub definitions
    * sub calls
    * string concat
    * number literals as long as they don't look like versions
    * return statements
    * undef keyword
    
    ... not bad!

--- test.pl
+++ test.pl
@@ -1,4 +1,15 @@
-sub foo {
-    require strict;
+use v5.18;
+use strict;
+use JSON::PP;
+use warnings;
+use feature qw(
+    say
+);
+
+sub blorg {
+    say shift;
     return undef;
 }
+
+blorg(9932.2);
+blorg("foo" . "bar");

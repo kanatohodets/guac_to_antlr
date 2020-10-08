832a5fec3a821777d3b386e387483485095b48b4
Ben Tyler <benjamin.tyler@gmail.com>
2020-10-08 23:27:39 +0200

    update test file. making progress!

--- test.pl
+++ test.pl
@@ -6,9 +6,22 @@ use feature qw(
     say
 );
 
+BEGIN {
+    warn "omg";
+}
+
+my $bar = rand();
+my $array_ref = ['a', "blorg $bar", qr/match/];
+my $hash_ref = { $bar => 5 };
+my %hash = ('what' => "is this");
+my @list = ($array_ref, $array_ref, $hash_ref, %hash);
+my $var_not_named_scalar = 'abc';
+
 sub blorg {
+    my $arg = shift();
     say shift;
-    return undef;
+    shift();
+    return $arg + 53;
 }
 
 blorg(9932.2);
